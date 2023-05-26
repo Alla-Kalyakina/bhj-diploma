9343d2335d6d265f49ecf47b635ba787c2c2edd7
Alla Kalyakina <alla.platonova95@yandex.ru>
2023-05-26 12:44:28 +0300

    edit createRequest.js

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,41 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+    const xhr = new XMLHttpRequest,
+  formData = new FormData;
+    let url = options.url;
 
+    if (options.method === 'GET') {
+      if (options.data) {
+         url = options.url + '?';
+         for (let prop in options.data) {
+            url += prop + '=' + options.data[prop] + '&';
+          }
+         url = url.substring(0, url.length - 1);
+      }
+    } else {
+        for (let prop in options.data) {           
+            formData.append(prop, options.data[prop]);
+        }
+    }
+
+    xhr.responseType = 'json';
+
+    try {
+        xhr.open(options.method, url);
+        xhr.send(formData);
+    } catch (err) {
+        options.callback(err); 
+    }
+
+    xhr.onload = () => {
+        if(xhr.response) {
+            if(xhr.response.success) {
+                options.callback(null, xhr.response);
+            }
+            else {
+                options.callback(xhr.response.error);
+            }
+        }    
+    }
 };
